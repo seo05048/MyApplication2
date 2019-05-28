a94ab73d91253f5152e2c0113f710432f66682fb
Jungmin <ljm9748@naver.com>
2019-05-29 00:43:30 +0900

    spinner commit

--- app/src/main/java/com/example/myapplication/JoinActivity.java
+++ app/src/main/java/com/example/myapplication/JoinActivity.java
@@ -2,14 +2,23 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class JoinActivity extends AppCompatActivity {
-
+    private ArrayAdapter adapter;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        spinner= (Spinner)findViewById(R.id.major);
+        adapter= ArrayAdapter.createFromResource(this, R.array.major, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
     }
 
 }
